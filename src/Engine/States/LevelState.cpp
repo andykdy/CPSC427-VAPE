8ba0869976308512fe63e25a0160bf7c89f8c4a9
Sam Parhimchik <s.parhim@me.com>
2019-11-04 10:37:58 -0800

    vamp mode explosions

--- src/Engine/States/LevelState.cpp
+++ src/Engine/States/LevelState.cpp
@@ -191,6 +191,7 @@ void LevelState::update(float ms) {
     {
         if (m_player->is_alive() && m_player->collides_with(*fish_it))
         {
+
             fish_it = m_fish.erase(fish_it);
             m_player->light_up();
             Mix_PlayChannel(-1, m_player_eat_sound, 0);
@@ -243,6 +244,7 @@ void LevelState::update(float ms) {
         while (turtle_it != m_turtles->end()) {
             if (m_vamp.collides_with(**turtle_it)) {
                 ECS::EntityId id = (*turtle_it)->getId();
+                m_explosion.spawn((*turtle_it)->get_position());
                 turtle_it = m_turtles->erase(turtle_it);
                 GameEngine::getInstance().getEntityManager()->removeEntity(id);
                 add_health(VAMP_HEAL);
--- src/Engine/States/TutorialState.cpp
+++ src/Engine/States/TutorialState.cpp
@@ -203,6 +203,7 @@ void TutorialState::update(float ms) {
 		turtle_it = m_turtles.begin();
 		while (turtle_it != m_turtles.end()) {
 			if (m_vamp.collides_with(**turtle_it)) {
+                m_explosion.spawn((*turtle_it)->get_position());
 				turtle_it = m_turtles.erase(turtle_it);
 				add_health(VAMP_HEAL);
 				m_vamp_quota--;
