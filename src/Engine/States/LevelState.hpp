27678bfdee499c37b9828c54e79c46067373dd6a
Andy Kim <andykdy@students.cs.ubc.ca>
2019-11-08 15:08:54 -0800

    Tutorial enhancement v1. Shift to cont and vamp mode changes

--- src/Engine/States/LevelState.hpp
+++ src/Engine/States/LevelState.hpp
@@ -18,7 +18,7 @@
 #include "Entities/fish.hpp"
 #include "Entities/Space.hpp"
 #include "Entities/bullet.hpp"
-#include "Entities/Dialogue.hpp"
+#include "Entities/UI/Dialogue.hpp"
 #include "Entities/UI/Health.hpp"
 #include "Entities/Explosion.hpp"
 
