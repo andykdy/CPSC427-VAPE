0d8a4939d5193766e1f049c9c77c5a2c80f6963c
Cody Newman <ctman96@ggmail.com>
2019-11-09 14:05:05 -0800

    fix

--- src/Entities/UI/Continue.cpp
+++ src/Entities/UI/Continue.cpp
@@ -69,7 +69,7 @@ void Continue::destroy()
 {
 	glDeleteBuffers(1, &mesh.vbo);
 	glDeleteBuffers(1, &mesh.ibo);
-	glDeleteBuffers(1, &mesh.vao);
+	glDeleteVertexArrays(1, &mesh.vao);
 
 	glDeleteShader(effect.vertex);
 	glDeleteShader(effect.fragment);
