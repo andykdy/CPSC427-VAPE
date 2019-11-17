1a5bf6c34d7d2d4ab1bbede83442f049b0082a36
ctman96 <ctman96@students.cs.ubc.ca>
2019-11-17 15:18:47 -0800

    Attempt to add ffmpeg to build

---
+++ src/Engine/Graphics/VideoUtil.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cody on 11/17/2019.
+//
+
+#include "VideoUtil.hpp"
---
+++ src/Engine/Graphics/VideoUtil.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Cody on 11/17/2019.
+//
+
+#ifndef VAPE_VIDEOUTIL_HPP
+#define VAPE_VIDEOUTIL_HPP
+
+
+class VideoUtil {
+
+};
+
+
+#endif //VAPE_VIDEOUTIL_HPP
