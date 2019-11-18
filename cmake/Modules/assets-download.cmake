5adffe0e62c355a02e9208914cb51b1925fa4523
Cody Newman <ctman96@ggmail.com>
2019-11-18 02:39:42 -0800

    Mac fix

--- cmake/Modules/assets-download.cmake
+++ cmake/Modules/assets-download.cmake
@@ -2,9 +2,12 @@ cmake_minimum_required(VERSION 3.10)
 
 function(download_assets)
         # TODO some way of checking if file is up to date?
-        file(DOWNLOAD https://gdurl.com/Gn-e/_/4946ee32a31e712ab7a1fb7cf6aa265e ${CMAKE_CURRENT_SOURCE_DIR}/data/data.7z SHOW_PROGRESS)
-        execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xfvz ${CMAKE_CURRENT_SOURCE_DIR}/data/data.7z
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/data)
+        # TODO move onto my home server for faster speeds?
+        if (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/data/data.7z)
+                file(DOWNLOAD https://cdn-16.anonfile.com/Y8f2deB3n1/a07f9851-1574074243/data.7z ${CMAKE_CURRENT_SOURCE_DIR}/data/data.7z SHOW_PROGRESS)
+                execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xfvz ${CMAKE_CURRENT_SOURCE_DIR}/data/data.7z
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/data)
+        endif()
 endfunction(download_assets)
 
 #[[
