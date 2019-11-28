b3ec1ca3279f24594d3b480b4ca480f4bd1b76ca
Andy Kim <andykdy@students.cs.ubc.ca>
2019-11-27 17:30:41 -0800

    Applying Cody's change

--- src/Engine/Graphics/Font.cpp
+++ src/Engine/Graphics/Font.cpp
@@ -8,13 +8,13 @@
 #include <stb_truetype.h>
 #include <iostream>
 
+unsigned char fontdata[1 << 20];
+unsigned char temp_bitmap[512 * 512];
+
 Font::Font(const char *path) {
     m_path = std::string(path);
 
-    unsigned char fontdata[1<<19];
-    unsigned char temp_bitmap[512*512];
-
-    fread(fontdata, 1, 1<<19, fopen(path, "rb"));
+    fread(fontdata, 1, 1<<20, fopen(path, "rb"));
     stbtt_BakeFontBitmap(fontdata,stbtt_GetFontOffsetForIndex(fontdata, 0), 32.0, temp_bitmap,512,512, 32,96, cdata);
     // can free ttf_buffer at this point
     glGenTextures(1, &m_tex);
