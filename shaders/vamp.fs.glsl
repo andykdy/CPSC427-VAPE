25eb62d17ac48dedd3d48ba3021cebecc779a21f
Matteo Alps-Mocellin <mattm177@students.cs.ubc.ca>
2019-12-05 12:12:40 -0800

    Fixed transparent menu bug

--- shaders/vamp.fs.glsl
+++ shaders/vamp.fs.glsl
@@ -13,9 +13,5 @@ layout(location = 0) out  vec4 color;
 
 void main()
 {
-    if (falpha != 0) {
-        color = vec4(fcolor, falpha) * texture(sampler0, vec2(texcoord.x, texcoord.y));
-    } else {
-        color = vec4(fcolor, 1) * texture(sampler0, vec2(texcoord.x, texcoord.y));
-    }
+    color = vec4(fcolor, falpha) * texture(sampler0, vec2(texcoord.x, texcoord.y));
 }
