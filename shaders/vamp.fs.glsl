10b63c400a734076e2c07e2f8314fa6d09e5d22e
Matteo Alps-Mocellin <mattm177@students.cs.ubc.ca>
2019-12-05 01:21:39 -0800

    Vamp activation cost removed, vamp displays charge left via alpha

--- shaders/vamp.fs.glsl
+++ shaders/vamp.fs.glsl
@@ -6,11 +6,16 @@ in vec2 texcoord;
 // Application data
 uniform sampler2D sampler0;
 uniform vec3 fcolor;
+uniform float falpha;
 
 // Output color
 layout(location = 0) out  vec4 color;
 
 void main()
 {
-    color = vec4(fcolor, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
+    if (falpha != 0) {
+        color = vec4(fcolor, falpha) * texture(sampler0, vec2(texcoord.x, texcoord.y));
+    } else {
+        color = vec4(fcolor, 1) * texture(sampler0, vec2(texcoord.x, texcoord.y));
+    }
 }
